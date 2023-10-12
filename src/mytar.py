8b25bd4a9c24adeff31bf5e3fd65525f0fcb28ec
aswatson <aswatson95@gmail.com>
2023-10-12 00:29:52 -0600

    this is current

--- src/mytar.py
+++ src/mytar.py
@@ -1,34 +1,38 @@
-#! /user/bin/python3
+#!/usr/bin/env python3
 
-#! usr/bin/env python3
+import os
+import sys
 
-import os, sys      
+def create_archive(output_file, data):
+    for entry in data:
+        name = entry[0]
+        content = entry[1]
+        byte = f"{name}\n{content}\n".encode()
+        output_file.write(byte)
+
+def extract_archive(input_file):
+    data = input_file.read().decode()
+    lines = data.strip().split('\n')
+    
+    for i in range(0, len(lines), 2):
+        name = lines[i]
+        content = lines[i + 1]
+        print(f"Extracting file: {name}")
+        print(f"File content: {content}")
 
 if len(sys.argv) < 3:
     print("Appropriate command is mytar <command> <filename> <command line arg> <filename>")
     exit()
+
 command = sys.argv[1]
 
 if command == 'c':
-    for arg in sys.argv:
-        if arg != 'c' and arg != './mytar.py':
-            data = b''
-            inputFile = os.open(arg, os.O_RDONLY)
-            read = os.read(inputFile, 100)
-            while read != b'':
-                data = data+read
-                read = os.read(inputFile, 100)
-            byte = str(len(arg))+':'+arg+str(len(data))+':'
-            byte = byte.encode()
-            byte = byte + data
-            os.write(1, byte)
-            os.close(inputFile)
-
-if command == 'x':
-    inputFile = os.open(sys.argv[2], os.O_RDONLY)
-    data = ''
-    read = os.read(inputFile, 100).decode()
-    while read != '':
-        data = data+read
-        read = os.read(inputFile, 100).decode()
-    lines = data.split('\n')
+    if len(sys.argv) < 4:
+        print("Usage: mytar c <string1> <string2> ... > output.txt")
+    else:
+        data = [(arg, arg.encode()) for arg in sys.argv[2:]]
+        create_archive(sys.stdout, data)
+elif command == 'x':
+    extract_archive(sys.stdin)
+else:
+    print("Invalid command: Use 'c' for create or 'x' for extract.")
