0f8320cd791c18aaf1b77d10ed6d35411b4a9f81
aswatson <aswatson95@gmail.com>
2023-10-12 11:41:14 -0600

    efficiency standards

--- src/mytar.py
+++ src/mytar.py
@@ -3,56 +3,22 @@
 import os
 import sys
 
-def create_archive(files):
-    for file in files:
-        try:
-            file_to_read = os.open(file, 'r')
-            static_data = os.fstat(file_to_read)
-            meta_data = encode_metadata(static_data)
-            sys.stdout.write(meta_data)
+def create_archive(file_descriptor):
+    data = sys.argv[2,-2,:]
+    fd = os.open(fd, 100)
 
-            while static_data > 0:
-                chunk = os.read(file_to_read, chunk_size)
-                if not chunk:
-                    break
-                sys.stdout.write(chunk)
-            
-            os.close(file_to_read)
 
-        except Exception as e:
-            #Handle whatever exception errors arise (change as exceptions are found)
-            sys.stderr.write(f"Error while processing {file}: {str(e)}")
+def extract_archive(file_descriptor):
+    data = os.open(fd, 100)
 
-def extract_archive():
-    while True:
-        try:
-            meta_data = sys.stdin.read(meta_data.__sizeof__)
-            if not meta_data:
-                break
-                
-            static_data = decode_metadata(meta_data)
+if len(sys.argv) < 3:
+    print(f"Usage: ./mytar.py <command> <pointer> <filename")
+    exit(1)
 
-            file_to_read = os.open(static_data.filename, os.O_CREAT | os.O_WRONLY | os.O_TRUNC)
+command = sys.argv[1]
+fd = sys.argv[-1]
 
-            while static_data.size > 0:
-                chunk_size = min(static_data.size, chunk_size)
-                chunk = sys.stdin.read(chunk_size)
-                os.write(file_to_read, chunk)
-                static_data.size -= len(chunk)
-
-            os.close(file_to_read)
-
-        except Exception as e:
-            sys.stderr.write(f"Error while processing {file}: {str(e)}")
-
-def encode_metadata(static_data):
-    pass
-
-def decode_metadata(meta_data):
-    pass
-
-def main(self):
-    if(sys.argv.__len__<3):
-        print(f"Error in formatting")
-        exit(0)
-    
+if command == "c":
+    create_archive(fd)
+elif command == "x":
+    extract_archive(fd)
