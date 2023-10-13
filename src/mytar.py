f5412b0b84d4e37a6958a77be46dab25255b3daf
aswatson <aswatson95@gmail.com>
2023-10-13 08:35:35 -0600

    implemented buffer

--- src/mytar.py
+++ src/mytar.py
@@ -1,11 +1,62 @@
 #! /usr/bin/env python3
 
-import os, sys      
+import os
+import sys 
 
+class BufferedFdReader:
+    def __init__(self, fd, bufLen = 1024*16):
+        self.fd = fd
+        self.buf = b""
+        self.index = 0
+        self.bufLen = bufLen
+    def readByte(self):
+        if self.index >= len(self.buf):
+            self.buf = os.read(self.fd, self.bufLen)
+            self.index = 0
+        if len(self.buf) == 0:
+            return None
+        else:
+            retval = self.buf[self.index]
+            self.index += 1
+            return retval
+    def close(self):
+        os.close(self.fd)
+
+class BufferedFdWriter:
+    def __init__(self, fd, bufLen = 1024*16):
+        self.fd = fd
+        self.buf = bytearray(bufLen)
+        self.index = 0
+    def writeByte(self, bVal):
+        self.buf[self.index] = bVal
+        self.index += 1
+        if self.index >= len(self.buf):
+            self.flush()
+    def flush(self):
+        startIndex, endIndex = 0, self.index
+        while startIndex < endIndex:
+            nWritten = os.write(self.fd, self.buf[startIndex:endIndex])
+            if nWritten == 0:
+                os.write(2,f"buf.BufferedFdWriter(fd={self.fd}): flush failed\n".encode())
+                sys.exit(1)
+            startIndex += nWritten
+        self.index = 0
+    def close(self):
+        self.flush()
+        os.close(self.fd)
+
+def createArchive(file):
+    pass
+
+def extractArchive(fiel):
+    pass
 
 command = sys.argv[1]
 
 if command == "c":
-    create_archive(fd)
+    file = sys.argv[2:]
+    createArchive(file)
 elif command == "x":
-    extract_archive(fd)
+    file = sys.argv[2]
+    extractArchive(file)
+
