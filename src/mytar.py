8abd3395185836820cf4ee6f4fdb962a8ea1ebc3
aswatson <aswatson95@gmail.com>
2023-10-13 07:58:08 -0600

    set up for buffered writer conversion

--- src/mytar.py
+++ src/mytar.py
@@ -2,20 +2,8 @@
 
 import os, sys      
 
-def create_archive(file_descriptor):
-    data = sys.argv[2,-3]
-    fd = os.open(fd, 100)
-
-
-def extract_archive(file_descriptor):
-    data = os.read(fd, 100)
-
-if len(sys.argv) < 2:
-    print(f"Usage: ./mytar.py <command> <pointer> <filename>")
-    exit(1)
 
 command = sys.argv[1]
-fd = sys.argv[-1]
 
 if command == "c":
     create_archive(fd)
